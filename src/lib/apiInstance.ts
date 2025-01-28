4fe8ed2286b90aac5491615ab32de7dd4823804f
programmerhasan001 <programmermdhasan@gmail.com>
2025-01-28 15:11:21 +0600

    app urls

--- src/lib/apiInstance.ts
+++ src/lib/apiInstance.ts
@@ -6,7 +6,8 @@ const token = localStorage.getItem("access_token") || "";
 console.log(token, "access_token sss");
 
 export const apiInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://phplaravel-1241841-5214027.cloudwaysapps.com/api",
   headers: {
     Authorization: `Bearer ${token.replace(/"/g, "")}`,
   },
